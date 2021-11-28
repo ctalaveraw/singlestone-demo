bef60e2ee209e7de51aaa7a8dc6911b76ba65a1a
ctalaveraw <ctalaveraw@gmail.com>
2021-11-27 23:01:01 -0500

    fixed error in data source file

--- project_code/exercise_1/deployment/data.tf
+++ project_code/exercise_1/deployment/data.tf
@@ -1,7 +1,7 @@
 ## This is the data source that holds the Python application source code
 data "archive_file" "lambda_zip" {
   type             = "zip"
-  source_file       = var.app_code_path
   output_file_mode = "0666"
+  source_dir       = var.app_code_path
   output_path      = var.app_code_output
 }
