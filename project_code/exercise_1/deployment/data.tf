303a2be79187b39a662120199d5bcb53df679a87
ctalaveraw <ctalaveraw@gmail.com>
2021-11-27 22:30:31 -0500

    added Lambda source code paths to *.tfvars file

--- project_code/exercise_1/deployment/data.tf
+++ project_code/exercise_1/deployment/data.tf
@@ -1,7 +1,7 @@
 ## This is the data source that holds the Python application source code
 data "archive_file" "lambda_zip" {
   type             = "zip"
-  source_dir       = "source_code"
+  source_file       = var.app_code_path
   output_file_mode = "0666"
-  output_path      = "lambda_fortune_app.zip"
+  output_path      = var.app_code_output
 }
--- project_code/exercise_1/deployment/variables.tf
+++ project_code/exercise_1/deployment/variables.tf
@@ -7,17 +7,28 @@ variable "lambda_function_name" {
   default     = "lambda-random-fortune"
 }
 
-## The vairables for granting Terraform access to AWS is defined here
-## Actual values are stored in a *.tfvars file
-## Create a custom *.tfvars file for personal deployments
+
+/*
+Actual values are stored in a *.tfvars file
+Create a custom *.tfvars file for personal deployments
+*/
+
+## The variables for granting Terraform access to AWS are defined here
 variable "aws_access_key" {
   description = "AWS access key; for assuming an identity for resource deployment."
   type        = string
 }
-
 variable "aws_secret_key" {
   description = "AWS secret key; for verifying identity for resource deployment."
   type        = string
 }
 
-  
+## The variables representing the Lambda app source code paths are defined here
+variable "app_code_path" {
+  description = "The path where the Lambda Python app source code is located"
+  type        = string
+}
+variable "app_code_output" {
+  description = "The path where the Lambda Python app package is outputted"
+  type        = string
+}
